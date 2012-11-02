65fc75cee84068c9c0ed01ef83ad19c0435589f9
adam <anton@adamansky.com>
2012-11-03 00:42:00 +0700

    #1

--- tests.mk
+++ tests.mk
@@ -1,8 +1,13 @@
 
-tests :
+check:
 	- mkdir -p var
-	##make -C ./tcejdb check-ejdb
+	make -C ./tcejdb check-ejdb
+	nodeunit ./node/tests
+
+check-all:
+	- mkdir -p var
+	make -C ./tcejdb check
 	nodeunit ./node/tests
 
 
-.PHONY: tests
+.PHONY: check check-all
