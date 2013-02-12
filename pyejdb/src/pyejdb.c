3164e74f6517de7dcd7b664ad510eb4056b5e56f
adam <adamansky@gmail.com>
2013-02-12 23:20:20 +0700

    #47

--- pyejdb/src/pyejdb.c
+++ pyejdb/src/pyejdb.c
@@ -6,6 +6,41 @@ typedef struct {
     EJDB *ejdb;
 } PEJDB;
 
+/* Cursor */
+typedef struct {
+    PyObject_HEAD
+    PyObject *db;
+    TCLIST *res;
+    int pos;
+} PDBCursor;
+
+static PyObject* PDBCursor_tp_new(PyTypeObject *type, PyObject *db, TCLIST *res) {
+    PDBCursor *self = (PDBCursor *) type->tp_alloc(type, 0);
+    if (!self) {
+        return NULL;
+    }
+    Py_INCREF(db);
+    self->db = db;
+    self->res = res;
+    self->pos = 0;
+    return (PyObject *) self;
+}
+
+static int PDBCursor_tp_clear(PDBCursor *self) {
+    Py_CLEAR(self->db);
+    return 0;
+}
+
+static void PDBCursor_tp_dealloc(PDBCursor *self) {
+    if (self->res) {
+        tclistdel(self->res);
+        self->res = NULL;
+    }
+    PDBCursor_tp_clear(self);
+    Py_TYPE(self)->tp_free((PyObject *) self);
+}
+
+
 #include "EJDB.c"
 
 PyDoc_STRVAR(ejdb_m_doc, "EJDB http://ejdb.org");
