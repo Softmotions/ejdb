ae51ac621b1e6342c2223a006a7b1d83e2e690a2
adam <adamansky@gmail.com>
2013-08-20 01:41:21 +0700

    build automation

--- tcejdb/tcutil.h
+++ tcejdb/tcutil.h
@@ -3733,7 +3733,7 @@ typedef unsigned char TCBITMAP; /* type of a bit map object */
 
 #include <stdio.h>
 
-#define _TC_VERSION    "1.1.19"
+#define _TC_VERSION    "1.1.20"
 #define _TC_LIBVER     911
 #define _TC_FORMATVER  "1.0"
 
---
+++ tools/release.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DEBUG=true
+EJDB_HOME=$(cd $(dirname ${0})/..; pwd -P)
+PPA=ppa:adamansky/ejdb
+
+function error() { echo "ERROR $*"; }
+
+function tcejdb() {
+    TCEJDB_HOME="${EJDB_HOME}/tcejdb"
+    VERSION="$1"
+    if [ -z ${VERSION} ]; then
+       error "$VERSION";
+    fi
+    cd ${TCEJDB_HOME}
+    autoconf
+    CPKG=$(cat configure.ac | grep AC_INIT| sed 's/.*(\(.*\)).*/\1/'| awk -F, '{print $1}')
+    CVERSION=$(cat configure.ac | grep AC_INIT| sed 's/.*(\(.*\)).*/\1/'| awk -F, '{print $2}');
+    if [[ ${VERSION} != ${CVERSION} ]]; then
+        echo "Updating configure.ac version"
+        sed -i 's/AC_INIT( *'"$CPKG"' *, *'"$CVERSION"' */AC_INIT('"$CPKG"','"$VERSION"'/' configure.ac
+        autoconf
+        dch -Dtesting -v"${VERSION}" || exit $?
+        make -C "${EJDB_HOME}" deb-source-packages-tcejdb
+        dput -c tools/dput-raring.cf "${PPA}" "./libtcejdb_${VERSION}_source.changes"
+    else
+        echo "The configure.ac version up-to-date"
+        exit 0;
+    fi
+
+}
+
+
+tcejdb $*
