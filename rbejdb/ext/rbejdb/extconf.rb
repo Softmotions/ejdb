c39b980928cde615a2719af759f5e1fd7a543d30
adam <adamansky@gmail.com>
2013-09-13 22:33:40 +0700

    rbejdb moved into ejdb-ruby repo

--- rbejdb/ext/rbejdb/extconf.rb
+++
@@ -1,9 +0,0 @@
-require "mkmf"
-
-unless have_library('tcejdb')
-  raise "EJDB C library is not installed!"
-end
-
-$CFLAGS << ' -Wall'
-CONFIG['warnflags'].gsub!('-Wdeclaration-after-statement', '')
-create_makefile("rbejdb", ARGV[0] || 'src')
