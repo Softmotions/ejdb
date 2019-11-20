1d89cff4d4de88e7ff7f0d77afa314012261fe44
Anton Adamansky <adamansky@gmail.com>
2019-11-21 00:22:08 +0700

    * #267

--- src/bindings/ejdb2_swift/EJDB2Swift/Package.swift
+++ src/bindings/ejdb2_swift/EJDB2Swift/Package.swift
@@ -5,14 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "EJDB2Swift",
-    // products: [
-    //     .library(
-    //         name: "EJDB2Swift",
-    //         targets: ["EJDB2Swift"]),
-    // ],
+    products: [
+        .library(
+            name: "EJDB2Swift",
+            targets: ["EJDB2Swift"]),
+        .executable(
+            name: "EJDB2SwiftRun",
+            targets: ["EJDB2SwiftRun"]
+        )
+    ],
     targets: [
         .systemLibrary(name: "EJDB2", pkgConfig: "libejdb2"),
         .target(name: "EJDB2Swift", dependencies: ["EJDB2"]),
+        .target(name: "EJDB2SwiftRun", dependencies: ["EJDB2Swift"]),
         .testTarget(name: "EJDB2SwiftTests", dependencies: ["EJDB2Swift"]),
     ]
 )
