482955eaa86bdc2871392fc170bad968aa1ad119
Anton Adamansky <adamansky@gmail.com>
2019-11-26 00:17:31 +0700

    * #267

--- src/bindings/ejdb2_swift/EJDB2Swift/Sources/EJDB2Swift/EJDB2Swift.swift
+++ src/bindings/ejdb2_swift/EJDB2Swift/Sources/EJDB2Swift/EJDB2Swift.swift
@@ -1,9 +1,9 @@
 import EJDB2
 
-public struct EJDB2Swift {
+public enum EJDB2Swift {
 
   public static var versionFull: String {
-    return String(cString: ejdb_version_full());
+    return String(cString: ejdb_version_full())
   }
 
   public static var version: (major: Int, minor: Int, patch: Int) {
@@ -14,10 +14,66 @@ public struct EJDB2Swift {
 /// EJDB2 document items
 public struct JBDOC {
   let id: Int64
-  let json: String;
+  let json: String
 
   public init(id: Int64, json: String) {
     self.id = id
     self.json = json
   }
+}
+
+/// EJDB2 instance builder
+public struct EJDB2Builder {
+
+  /// Database path
+  let path: String
+
+  /// Open database in readonly mode
+  var readonly: Bool = false
+
+  /// Truncate database on open
+  var truncate: Bool = false
+
+  /// WAL enabled
+  var walEnabled = true
+
+  var walCheckCRCOnCheckpoint: Bool?
+
+  var walCheckpointBufferSize: Int?
+
+  var walCheckpointTimeout: Int?
+
+  var walSavepointTimeout: Int?
+
+  var walBufferSize: Int?
+
+  var documentBufferSize: Int?
+
+  var sortBufferSize: Int?
+
+  init(path: String) {
+    self.path = path
+  }
+
+  mutating func withReadonly() -> EJDB2Builder {
+    readonly = true
+    return self
+  }
+
+  mutating func withTruncate() -> EJDB2Builder {
+    truncate = true
+    return self
+  }
+
+  mutating func withWalDisabled() -> EJDB2Builder {
+    walEnabled = false
+    return self
+  }
+}
+
+/// EJDB2
+public class EJDB2 {
+
+  internal private(set) var handle: OpaquePointer?
+
 }
--- src/bindings/ejdb2_swift/EJDB2Swift/Tests/EJDB2SwiftTests/EJDB2SwiftTests.swift
+++ src/bindings/ejdb2_swift/EJDB2Swift/Tests/EJDB2SwiftTests/EJDB2SwiftTests.swift
@@ -1,15 +1,30 @@
+import Foundation
 import XCTest
+
+
 @testable import EJDB2Swift
 
 final class EJDB2SwiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EJDB2Swift().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  override func setUp() {
+    super.setUp()
+  }
+
+  override func tearDown() {
+    super.tearDown()
+  }
+
+  func testMain() {
+
+
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    //XCTAssertEqual(EJDB2Swift().text, "Hello, World!")
+
+  }
+
+  static var allTests = [
+    ("testMain", testMain),
+  ]
 }
