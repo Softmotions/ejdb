c0d4069a5323fec081bb5ed9e6311e41472db354
Anton Adamansky <adamansky@gmail.com>
2019-12-19 21:18:14 +0700

    * #267

--- src/bindings/ejdb2_swift/EJDB2Swift/Package.swift
+++ src/bindings/ejdb2_swift/EJDB2Swift/Package.swift
@@ -4,20 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "EJDB2Swift",
-    products: [
-        .library(
-            name: "EJDB2Swift",
-            targets: ["EJDB2Swift"]),
-        .executable(
-            name: "EJDB2SwiftRun",
-            targets: ["EJDB2SwiftRun"]
-        )
-    ],
-    targets: [
-        .systemLibrary(name: "EJDB2", pkgConfig: "libejdb2"),
-        .target(name: "EJDB2Swift", dependencies: ["EJDB2"]),
-        .target(name: "EJDB2SwiftRun", dependencies: ["EJDB2Swift"]),
-        .testTarget(name: "EJDB2SwiftTests", dependencies: ["EJDB2Swift"]),
-    ]
+  name: "EJDB2Swift",
+  products: [
+    .library(
+      name: "EJDB2Swift",
+      targets: ["EJDB2Swift"]),
+    .executable(
+      name: "EJDB2SwiftRun",
+      targets: ["EJDB2SwiftRun"]
+    ),
+  ],
+  dependencies: [
+    //.package(url: "https://github.com/Softmotions/SwiftCoroutine", from: "1.0.1"),
+  ],
+  targets: [
+    .systemLibrary(name: "EJDB2", pkgConfig: "libejdb2"),
+    .target(
+      name: "EJDB2Swift",
+      dependencies: [
+        "EJDB2",
+        //"SwiftCoroutine"
+      ]),
+    .target(name: "EJDB2SwiftRun", dependencies: ["EJDB2Swift"]),
+    .testTarget(name: "EJDB2SwiftTests", dependencies: ["EJDB2Swift"]),
+  ]
 )
