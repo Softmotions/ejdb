93e6db0f9ee59709ace6b63f689a1a762afcd9bd
Anton Adamansky <adamansky@gmail.com>
2020-01-07 00:27:41 +0700

    * 2019 -> 2020

--- src/bindings/ejdb2_react_native/binding/index.d.ts
+++ src/bindings/ejdb2_react_native/binding/index.d.ts
@@ -3,7 +3,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2012-2019 Softmotions Ltd <info@softmotions.com>
+ * Copyright (c) 2012-2020 Softmotions Ltd <info@softmotions.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
