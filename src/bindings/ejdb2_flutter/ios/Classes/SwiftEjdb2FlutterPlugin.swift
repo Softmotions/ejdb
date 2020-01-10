cb2b250a2f6d4f434bad2670e02adec7bb5a9d09
Anton Adamansky <adamansky@gmail.com>
2020-01-11 00:31:06 +0700

    #272

--- src/bindings/ejdb2_flutter/ios/Classes/SwiftEjdb2FlutterPlugin.swift
+++ src/bindings/ejdb2_flutter/ios/Classes/SwiftEjdb2FlutterPlugin.swift
@@ -1,14 +1,35 @@
 import Flutter
 import UIKit
+import EJDB2
 
-public class SwiftEjdb2FlutterPlugin: NSObject, FlutterPlugin {
+public class SwiftEjdb2FlutterPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ejdb2_flutter", binaryMessenger: registrar.messenger())
-    let instance = SwiftEjdb2FlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    SwiftEjdb2FlutterPlugin(registrar)
+  }
+
+  let methodChannel: FlutterMethodChannel
+  let eventChannel: FlutterEventChannel
+
+  init(_ registrar: FlutterPluginRegistrar) {
+    self.methodChannel = FlutterMethodChannel(name: "ejdb2", binaryMessenger: registrar.messenger())
+    self.eventChannel = FlutterEventChannel(name: "ejdb2/query", binaryMessenger: registrar.messenger())
+    super.init()
+    registrar.addMethodCallDelegate(self, channel: methodChannel)
+    eventChannel.setStreamHandler(self)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    // result("iOS " + UIDevice.current.systemVersion)
+  }
+
+  public func onListen(withArguments arguments: Any?, eventSink events: FlutterEventSink) -> FlutterError? {
+    //fatalError("onListen(withArguments:eventSink:) has not been implemented")
+    return nil
+  }
+
+  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    //fatalError("onCancel(withArguments:) has not been implemented")
+    return nil
   }
 }
--- src/bindings/ejdb2_flutter/ios/ejdb2_flutter.podspec
+++ src/bindings/ejdb2_flutter/ios/ejdb2_flutter.podspec
@@ -15,7 +15,8 @@ Embeddable JSON Database engine EJDB http://ejdb.org Flutter binding
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '8.0'
+  s.dependency 'EJDB2'
+  s.platform = :ios, '9.0'
 
   # Flutter.framework does not contain a i386 slice. Only x86_64 simulators are supported.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
