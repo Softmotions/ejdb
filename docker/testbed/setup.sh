1facb71014008435b10d4bd75245d4158b2e21bb
Anton Adamansky <adamansky@gmail.com>
2020-05-20 03:28:33 +0700

    * Minors

--- docker/testbed/setup.sh
+++ docker/testbed/setup.sh
@@ -14,9 +14,7 @@ cd $SCRIPTPATH
 JENKINS_AGENT_VERSION=4.3
 
 dpkg --add-architecture i386
-apt-get update
 apt-get install -y apt-utils software-properties-common apt-transport-https sudo curl wget zip unzip
-add-apt-repository ppa:carsten-uppenbrink-net/openssl
 apt-get update
 apt-get dist-upgrade -y
 
