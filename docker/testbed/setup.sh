e59cb08f5c2ff1ce8b00e958671b9d4f655d77b2
Anton Adamansky <adamansky@gmail.com>
2020-05-21 11:47:03 +0700

    * Docker testbed clang fixes

--- docker/testbed/setup.sh
+++ docker/testbed/setup.sh
@@ -27,7 +27,9 @@ wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ xenial main'
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list >/etc/apt/sources.list.d/dart_stable.list
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+./llvm.sh 10
+./llvm-update-alternatives.sh 10 1000
 
 apt-get update
 
