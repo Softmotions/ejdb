a11dc2d87113e4db6b47479d7ab86d7a3daa492d
Anton Adamansky <adamansky@gmail.com>
2020-12-22 13:48:25 +0700

    * Added dart unstable to testbed

--- docker/testbed/setup.sh
+++ docker/testbed/setup.sh
@@ -26,7 +26,7 @@ wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 
 apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ xenial main'
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list >/etc/apt/sources.list.d/dart_stable.list
+wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_unstable.list >/etc/apt/sources.list.d/dart_unstable.list
 
 ./llvm.sh 10
 ./llvm-update-alternatives.sh 10 1000
